ac9fd4aede1a26dad0b965da891ca79ccf2146ec
Muse <nique283@gmail.com>
2026-04-03 09:32:37 -0500

    changed form colour

--- Communityclinic/RegistrationForm.Designer.cs
+++ Communityclinic/RegistrationForm.Designer.cs
@@ -129,6 +129,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.Register);
             this.Controls.Add(this.txtConfirmpassword);
